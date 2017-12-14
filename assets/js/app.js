dafd1b9ea43d7b7f188385d7ed3adb960acfa325
AGuerrav <aaguerravaldes@gmail.com>
2017-12-14 16:59:29 -0300

    htmls& funciones& arreglos css

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,26 +1,40 @@
 $(document).ready(function () {
 
-})
-var idPin = 0;
-function savePin(){
-  var titulo = $('#tituloInput').val(); //variable para rescatar el valor del input del titulo
-  var descripcion = $('#descripcionInput').val(); //variable para rescatar el valor del input del descripcion
-  var url = $ ('#urlInput').val(); //variable para rescatar el valor URL del input para ingresar la imagen
+  //para que desaparezca la primera pantalla
+  setTimeout(function() {
+    $(".fill").fadeOut(0);
+    },5000);
+  //para que la pantalla contenido no este durante los primeros 5 segundos
+  setTimeout(function() {
+    $(".start2").fadeOut(0);
+  },0);
+  //para que la pantalla contenido vuelva luego de los primeros 5 segundos
+  setTimeout(function() {
+    $(".start2").fadeIn(0);
+  },5000);
+  //para inicializar mi pluggins para el input de las banderas
+  $("#demo").intlTelInput();
 
-  $("#pines").append("<div id='pin_" + idPin + "' class='pin col-md-3'>" +
-        //Este es el frente
-        "<div class='panel panel-success front'>" +
-        "<div class='panel-heading'>" +
-        "<h3 class='panel-title'>" + titulo + "</h3>" +
-        "</div>" +
-        "<div class = 'panel-body'>" +
-        "<a href='#' class='thumbnail'>" +
-        "<img src='" + url + "' alt='...'>" +
-        "</a>" +
-        "</div>" +
-        "<div class ='panel-footer'>" + descripcion + "</div> </div>" +
-        //Este es el panel detrás
-        "<div class='rear'>" +
-        "</div>" +
-        "</div>");
-};
+  //para validar que son 10 numeros en el numero telefonico y se habilite el boton NEXT
+  $('.numberPhone').keyup(function() {
+    //almacenar en la variable los numeros de telefono
+    var phone = $('.numberPhone').val();
+    //console.log(phone);
+    if(phone.length >9) {
+      $('.btn-next').removeAttr("disabled");
+   } else {
+     $('.btn-next').attr("disabled"," ");
+   }
+  })
+});
+//para generar el numero aleatorio cuando clickeo el boton NEXT
+var randomNum = Math.floor(Math.random()*1000)+100
+$(".btn-next").click(function(){
+ alert("tu codigo LAB" + randomNum );
+});
+//para reenviar un codigo
+var randomNum = Math.floor(Math.random()*1000)+100
+$(".btn-resend").click(function(){
+ alert("tu codigo LAB" + randomNum );
+});
+//
